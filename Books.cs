e432b5fc2ac5e39d2ddd0364cdd767ba55bb0938
Sverlio <User@DESKTOP-OB1B2K1>
2025-07-06 19:28:52 +0300

    konstruktor pochti

--- Books.cs
+++ Books.cs
@@ -8,5 +8,14 @@ namespace RealLibrary
 {
     internal class Books
     {
+        public int Id {  get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Count { get; set; }
+        public string AvbCounter { get; set; }
+
+
     }
 }
