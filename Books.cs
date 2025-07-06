71e5f85c4603e181faca21244cee4d0e913a8a69
Sverlio <User@DESKTOP-OB1B2K1>
2025-07-06 20:19:45 +0300

    metod1

--- Books.cs
+++ Books.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +14,60 @@ namespace RealLibrary
         public string Author { get; set; }
         public string Year { get; set; }
         public string Genre { get; set; }
-        public string Count { get; set; }
-        public string AvbCounter { get; set; }
+        public int Count { get; set; }
+        public int AvbCounter { get; set; }
+        public static List<Books> BookList = new List<Books>();
 
+        public Books(int id, string title, string author, string year, string genre, int count, int avbCounter)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Year = year;
+            Genre = genre;
+            Count = count;
+            AvbCounter = avbCounter;
+        }
+            public void AddBook()
+             { 
+             Console.WriteLine(" Напиши следните данни за книгата отделени с запетайки:  ");
+             Console.WriteLine("Заглавие на книгата, Автор, Година на издаване, Жанр, Брой копия от книгата");
+
+             string newBook = Console.ReadLine();
+             string[] newBookData = newBook.Split(',') ;
+             int counter = int.Parse(newBookData[4]);
+ 
+              while (counter > 0)
+              {
+                for (int i = 0; i < newBookData.Length; i++)
+                {
+                    
+                    Books book = new Books(0,null,null,null,null,0,0);
+                    book.Title = newBookData[0];
+                    book.Author = newBookData[1];
+                    book.Year = newBookData[2];
+                    book.Genre = newBookData[3];
+                    book.Count = counter;
+                    counter--;
+                    BookList.Add(book);
+                }
+              }
+            }
+
+        /* public static void PrintBooks()
+        {
+            if (BookList.Count <= 0)
+            {
+                Console.WriteLine("Няма добавени книги.");
+                return;
+            }
+
+            foreach (var b in BookList)
+            {
+                Console.WriteLine($" {b.Title},  {b.Author}, {b.Year},  {b.Genre},  {b.Count}");
+            }
+        }
+        */
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace RealLibrary
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            //test
+          /*  Books book = new Books(0, null, null, null, null, 0, 0);
+            book.AddBook();
+            Books.PrintBooks();
+            Console.ReadLine(); */
         }
     }
 }
